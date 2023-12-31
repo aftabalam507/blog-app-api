9d1d1a20fd91ac4a2cf85e2a100fc6fa2f2ba1b8
aftabalam507 <aftabalam1996jan@gmail.com>
2023-12-31 14:22:49 +0530

    first commit

---
+++ src/main/java/com/blog/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.payloads.UserDto;
+import com.blog.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	//Post- create user
+	@PostMapping("/")
+	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+		
+		UserDto createdDtoUser = this.userService.createUser(userDto);
+		return new ResponseEntity<>(createdDtoUser , HttpStatus.CREATED);
+	}
+}
