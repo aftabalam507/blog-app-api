ba6713cf0933f923333b963ef6d010ca391f6753
aftabalam507 <aftabalam1996jan@gmail.com>
2024-01-04 16:34:53 +0530

    exception handling added

---
+++ src/main/java/com/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.blog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.blog.payloads.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse> resourceNotFoundException(ResourceNotFoundException exception) {
+		String message = exception.getMessage();
+		ApiResponse response = new ApiResponse(message, false);
+		return new ResponseEntity<ApiResponse>(response, HttpStatus.NOT_FOUND);
+	}
+
+}
